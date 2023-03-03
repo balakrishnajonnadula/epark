25aa49426cb03c5296c7b48de8cfe0fbb5c39e2d
rajithaVendi <ra22185@corp.ojas-it.com>
2023-03-03 19:26:26 +0530

    view component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route,  Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AddProducts from "./components/AddProducts";
 import ProductList from "./components/ProductList";
+import ViewProduct from "./components/ViewProduct";
+import '/node_modules/react-toastify/dist/ReactToastify.min.css';
+
 
 function App() {
   return (
     <div className="">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/prouductlist" exact element={<ProductList />} />
-          <Route path="/" exact element={<AddProducts />} />
-        </Routes>
-      </BrowserRouter>
+      <ToastContainer >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/prouductlist" exact element={<ProductList />} />
+
+            <Route path="/" exact element={<AddProducts />} />
+            <Route path="/viewproduct" element={<ViewProduct />} />
+          </Routes>
+        </BrowserRouter>
+      </ToastContainer>
     </div>
   );
 }
